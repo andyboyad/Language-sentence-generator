1651b84bae7721efcde57454152c318ce42662d3
Andrew Boyadjiev <bcowbob2@gmail.com>
2020-06-11 20:25:29 -0700

    added more functionality

--- words.js
+++ words.js
@@ -1,11 +1,19 @@
+console.log(localStorage)
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 document.getElementById('inputfile') 
 .addEventListener('change', function() {   
         var fr=new FileReader(); 
+        var f = this.files[0]
         fr.onload=function(){ 
             document.getElementById('output') 
                     .textContent=fr.result; 
+            setLocalStorage(f.name, fr.result)
         } 
-          
         fr.readAsText(this.files[0]); 
     }) 
 
@@ -25,3 +33,27 @@ function loadFile(filePath) {
     }
     return result;
   }
+
+function setLocalStorage(file_name, text) {
+  counter=0
+  text=text.split("\n")
+  console.log(text)
+  console.log(text.length)
+  localStorage.setItem(file_name, text.length)
+  for(var s in text) {
+    console.log(JSON.stringify(text[s].split(" ")))
+    localStorage.setItem(file_name + " " + s, JSON.stringify(text[s].split(" ")))
+    console.log(JSON.parse(localStorage.getItem(file_name + " " + s)))
+  }
+  console.log(localStorage)
+  pickWord(file_name)
+}
+
+function pickWord(file_name) {
+  var nums = parseInt(localStorage.getItem(file_name))
+  console.log(nums)
+  var word_num = getRandomInt(nums)
+  var word = JSON.parse(localStorage.getItem(file_name + " " + word_num))
+  console.log(word)
+  return word
+}
